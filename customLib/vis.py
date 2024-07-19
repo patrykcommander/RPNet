2a0b71f6e5b35562be6c8bcc5046764cd5d2ba3c
patrykcommander <patryko@bydnet.pl>
2024-07-19 10:15:34 +0200

    LSTM model for R-wave prediction trained on the MITBIH dataset

--- customLib/vis.py
+++ customLib/vis.py
@@ -2,12 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #plotting the signal
+
 def plot_ecg(signal: np.ndarray, r_peaks=None, fs: int = 100, title = ""):
     t = [1/fs * x for x in range(len(signal))]
     plt.plot(figsize=(5,4))
     plt.plot(t, signal)
-    if isinstance(r_peaks, (np.ndarray)):
-        if r_peaks.shape[0] > 0:
+    if r_peaks is not None:
+        if r_peaks.shape[0] == signal.shape[0]:   # r_peaks passed as probability vector (len, ) / x is of shape (1, len)
+            plt.plot(t, r_peaks, "r-")
+            plt.legend(["ECG", "R peaks"], loc="lower right")
+        elif r_peaks.shape[0] > 0:                # r_peaks passed as indices
             r_peaks_time = r_peaks / fs
             plt.plot(r_peaks_time, signal[r_peaks], "rx")
             plt.legend(["ECG", "R peaks"], loc="lower right")
